b3e2fabe60561e774d3853055f4bbb396da5592e
Guido <guido.avvisati@gmail.com>
2020-05-30 17:48:30 +0200

    fixed slider mean and using external css

--- src/appdash/main.py
+++ src/appdash/main.py
@@ -7,90 +7,104 @@ import numpy as np
 
 from appdash.predictions import mtcars, preds, fit, cyl_enc
 
+"""
+dash apps are unstyled by default
+external CSS stylesheets
+"""
+external_stylesheets = [
+    "https://codepen.io/chriddyp/pen/bWLwgP.css",
+    {
+        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
+        "rel": "stylesheet",
+        "integrity": "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO",
+        "crossorigin": "anonymous",
+    },
+]
 
 # create an instance of a dash app
-app = dash.Dash(__name__)
-app.title = 'Predicting MPG'
-
-# dash apps are unstyled by default
-# this css I'm using was created by the author of Dash
-# and is the most commonly used style sheet
-app.css.append_css({
-    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
+app = dash.Dash(
+    __name__, external_scripts=None, external_stylesheets=external_stylesheets
+)
+app.title = "Predicting MPG"
 
 # I compute these up front to avoid having to
 # calculate thes twice
-unq_cyl = mtcars['cyl'].unique()
-unq_cyl.sort() # so it's in a nice order
-opts_cyl = [{'label': i, 'value': i} for i in unq_cyl]
+unq_cyl = np.sort(mtcars["cyl"].unique().astype(int)).astype(str)
+opts_cyl = [{"label": i, "value": str(i)} for i in unq_cyl]
 
 
-app.layout = html.Div([
-
-        html.H5('Displacement (in cubic inches):'),
-        html.Br(), html.Br(),
-        daq.Slider(
-            id='input-disp',
-            min=np.floor(mtcars['disp'].min()),
-            max=np.ceil(mtcars['disp'].max()),
-            step=.5,
-            dots=False,
-            handleLabel={"showCurrentValue": True,"label": "Value"},
-            value=np.floor(mtcars['disp'].mean())),
-
-        html.H5('Quarter mile time:'),
+app.layout = html.Div(
+    [
+        html.H5("Displacement (in cubic inches):"),
+        html.Br(),
         html.Br(),
         daq.Slider(
-            id='input-qsec',
-            min=np.floor(mtcars['qsec'].min()),
-            max=np.ceil(mtcars['qsec'].max()),
+            id="input-disp",
+            min=np.floor(mtcars["disp"].min()),
+            max=np.ceil(mtcars["disp"].max()),
+            step=0.5,
             dots=False,
-            handleLabel={"showCurrentValue": True,"label": "Value"},
-            step=.25,
-            value=np.floor(mtcars['disp'].mean())),
-
-        html.H5('Number of cylinders:'),
+            handleLabel={"showCurrentValue": True, "label": "Value"},
+            value=np.floor(mtcars["disp"].mean()),
+        ),
+        html.H5("Quarter mile time:"),
+        html.Br(),
+        daq.Slider(
+            id="input-qsec",
+            min=np.floor(mtcars["qsec"].min()),
+            max=np.ceil(mtcars["qsec"].max()),
+            dots=False,
+            handleLabel={"showCurrentValue": True, "label": "Value"},
+            step=0.25,
+            value=np.floor(mtcars["qsec"].mean()),
+        ),
+        html.H5("Number of cylinders:"),
         dcc.RadioItems(
-            id='input-cyl',
+            id="input-cyl",
             options=opts_cyl,
-            value=opts_cyl[0].get('value'),
-            labelStyle={'display': 'inline-block'}),
-
-        daq.ToggleSwitch(
-            id='input-am',
-            label='Has manual transmission',
-            value=False),
-
-        html.H2(id='output-prediction')
-])
+            value=opts_cyl[0].get("value"),
+            labelStyle={"display": "inline-block"},
+        ),
+        daq.ToggleSwitch(id="input-am", label="Has manual transmission", value=False),
+        html.H2(id="output-prediction"),
+    ]
+)
 
 
 # callback will watch for changes in inputs and re-execute when any
 # changes are detected.
 @app.callback(
-    dash.dependencies.Output('output-prediction', 'children'),
+    dash.dependencies.Output("output-prediction", "children"),
     [
-        dash.dependencies.Input('input-disp', 'value'),
-        dash.dependencies.Input('input-qsec', 'value'),
-        dash.dependencies.Input('input-cyl', 'value'),
-        dash.dependencies.Input('input-am', 'value')])
-def callback_pred(disp, qsec, cyl, am):
-    # pass values from the function on to our prediction function
-    # defined in setup
-    pred = preds(fit=fit,
-                 cyl_enc=cyl_enc,
-                 disp=disp,
-                 qsec=qsec,
-                 am=np.float64(am),
-                 cyl=cyl)
+        dash.dependencies.Input("input-disp", "value"),
+        dash.dependencies.Input("input-qsec", "value"),
+        dash.dependencies.Input("input-cyl", "value"),
+        dash.dependencies.Input("input-am", "value"),
+    ],
+)
+def callback_pred(disp: float, qsec: float, cyl: str, am: bool) -> str:
+    """Pass values from the UI on to our prediction function defined in
+    predictions.py.
+
+    Parameters
+    ----------
+    disp
+    qsec
+    cyl
+    am
+
+    Returns
+    -------
+    None
+    """
+
+    pred = preds(
+        fit=fit, cyl_enc=cyl_enc, disp=disp, qsec=qsec, am=np.float64(am), cyl=cyl
+    )
     # return a string that will be rendered in the UI
     return "Predicted MPG: {}".format(pred)
 
 
 # for running the app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
-
-def run():
-    return True
